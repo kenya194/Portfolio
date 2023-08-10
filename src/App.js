54a51e69bf2050085cbc77229a6617a9f91443cd
kenya194 <ernestbv553@gmail.com>
2023-08-10 14:25:36 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
-import Layout from './components'
+import Layout from './components/Layout';
 
 
 function App() {
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import './index.scss';
 
 
 const Layout = () => {
-    return <>Hello</>
+    return  <>Hello</>
 
 }
 
